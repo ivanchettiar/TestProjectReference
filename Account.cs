e0f127e5926e042b788d369361cb689a65b145d5
Ivan <ivanchettiar1@gmail.com>
2021-07-29 12:40:26 +0530

    first commit

---
+++ Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Account
+    {
+       int balance = 20000;
+        
+        public int Withdraw(int a)
+        {
+            return balance - a;
+            
+        }
+
+        public int Deposit(int a)
+        {
+            return balance + a;
+
+        }
+
+
+
+
+    }
+}
---
+++ Empl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Empl
+    {
+        ///<summary>
+        ///on 28 july
+        ///</summary>
+        public int Add(int a, int b)
+        {
+           return a + b;
+        }
+        public string Message(string b)
+        {
+            return "Hello " + b;
+        }
+    }
+}
